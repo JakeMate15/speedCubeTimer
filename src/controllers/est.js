c68f63f37593cc2c52eb0b3e3fcc49069d8969ce
JakeMate14 <erik.mofe51@gmail.com>
2023-06-21 22:51:17 -0600

    grafica

--- src/controllers/est.js
+++ src/controllers/est.js
@@ -6,7 +6,9 @@ function estadisticas(req,res){
         req.session.ao12 = req.session.bao12==2147483637? '-': formatTime(req.session.bao12);
 
         req.getConnection((err, conn) => {
-            conn.query('SELECT t.tiempo, promedio.promedio_tiempo FROM tmp t CROSS JOIN (  SELECT AVG(tiempo) AS promedio_tiempo FROM tmp WHERE idSesion = ?) promedio WHERE t.idSesion = ?', [req.session.idSesion,req.session.idSesion], (err, prom) => {
+            conn.query('SELECT tiempo, fecha, valido, idSesion, mezcla,promedio_tiempo  FROM tmp t CROSS JOIN (  SELECT AVG(tiempo) AS promedio_tiempo FROM tmp WHERE idSesion = ?) promedio WHERE t.idSesion = ?', [req.session.idSesion,req.session.idSesion], (err, prom) => {
+                
+                req.session.info = JSON.stringify(prom);
                 req.session.avg = formatTime(prom[0].promedio_tiempo);
                 res.render('plantillas/estadisticas', req.session);
             });
